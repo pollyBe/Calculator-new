55fab03500d8a7d5c1513d77a9d4bb10572855a9
Polina Belash <polinka.zhuravlinka@gmail.com>
2025-05-21 13:21:04 +0300

    feat: add commands and memory logic

---
+++ src/js/commands/memory.js
@@ -0,0 +1,23 @@
+export class Memory {
+  constructor() {
+    this.value = 0;
+  }
+
+  clear() {
+    this.value = 0;
+  }
+
+  add(val) {
+    if (isNaN(val)) return;
+    this.value += val;
+  }
+
+  subtract(val) {
+    if (isNaN(val)) return;
+    this.value -= val;
+  }
+
+  recall() {
+    return this.value;
+  }
+}
